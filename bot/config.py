0e67f46343ed8b3f176b265a02b8569226a543e1
GENABA <misaghnikkhaha@qrypty.com>
2026-07-16 15:54:29 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+with open(config_dir / "config.example.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
 # load .env config
